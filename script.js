b7c92133dc8166b50edbe58faa2b45ae9ca95145
Daniel <daniel.suter@transmax.com.au>
2019-01-07 17:34:22 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 <script>
 // just a test for now   
 
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello World!';
+  
 function ReturnTime(event) {
   var n = event.timeStamp;
   document.getElementById("demo").innerHTML = n;
