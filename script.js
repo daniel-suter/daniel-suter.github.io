02a96522fe36a04954196a255e95ba69ec3abb00
Daniel <daniel.suter@transmax.com.au>
2019-01-07 18:08:18 +1000

    Trying to get the script working

--- script.js
+++ script.js
@@ -3,6 +3,10 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello World!';
 
+document.querySelector('html').onclick = fuction() {
+  alert('stop it');
+}
+
 //function ReturnTime(event) {
 //  var n = event.timeStamp;
 //  document.getElementById("demo").innerHTML = n;
