2b07373ea4b87153143bbf8ff4943664d7397d9b
Daniel <daniel.suter@transmax.com.au>
2019-01-10 08:16:19 +1000

    test random number

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
+var random_number = Math.random();
+
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello wide wide wide world!';
+myHeading.textContent = 'Hello wide wide wide world & random_number';
 
 var myHeading2 = document.querySelector('h2');
 myHeading2.textContent = 'Rover Jazz hands!';
