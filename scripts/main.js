1efc95f2283e0ca86881f5b4b9c28c56b666f20a
Daniel <daniel.suter@transmax.com.au>
2019-01-10 07:20:15 +1000

    script working

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,8 @@
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
+
+document.querySelector('html').onclick = fuction() {
+  alert('stop it');
+}
+
+alert('Testing this stuff');
