2158e6a396703a3a22fa9ac36599012f7cef93cd
Daniel <daniel.suter@transmax.com.au>
2019-01-09 16:47:10 +1000

    test

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // just a test for now
+"use strict";
 
 var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello world!';
@@ -6,6 +7,9 @@ myHeading.textContent = 'Hello world!';
 document.querySelector('html').onclick = fuction() {
   alert('stop it');
 }
+
+
+alert('Testing this stuff');
 
 //function ReturnTime(event) {
 //  var n = event.timeStamp;
