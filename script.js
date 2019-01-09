ca3d3b9352fd6b8c616e7b615d559f70782af386
Daniel <daniel.suter@transmax.com.au>
2019-01-10 07:34:40 +1000

    test

---
+++ script.js
@@ -0,0 +1,6 @@
+var text_change = math.random()
+
+var myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello the big wide world! & text_change + text_change';
+
+alert('Testing this stuff');
