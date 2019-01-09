82e28c0e3b17e82be999224a65d1eaa68c361bf3
Daniel <daniel.suter@transmax.com.au>
2019-01-10 07:23:37 +1000

    script test

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
+var text_change = math.random()
+
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+myHeading.textContent = 'Hello world! & text_change + text_change';
 
 document.querySelector('html').onclick = fuction() {
   alert('stop it');
