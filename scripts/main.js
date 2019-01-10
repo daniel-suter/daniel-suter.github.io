bf7d0ddef3c824641ec2cf26239181816cfec665
Daniel <daniel.suter@transmax.com.au>
2019-01-10 14:40:27 +1000

    test

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
+var d = new Date();
+var datestring = d.getDate()  + "-" + (d.getMonth()+1) + "-" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes();
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello wide world' + Date(Mon 31 Jan 2000);
+myHeading.textContent = 'Hello wide world' + datestring;
 
 var myHeading2 = document.querySelector('h2');
 myHeading2.textContent = 'Rovers Jazz hands!' + Math.random();
