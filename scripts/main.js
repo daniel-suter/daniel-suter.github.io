638338e07c392e870b7cd99693882af71db5b9a2
Daniel <daniel.suter@transmax.com.au>
2019-01-10 10:28:49 +1000

    test

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ var myHeading = document.querySelector('h1');
 myHeading.textContent = 'Hello wide wide wide world';
 
 var myHeading2 = document.querySelector('h2');
-myHeading2.textContent = 'Rover Jazz hands!' & rand_number;
+myHeading2.textContent = 'Rover Jazz hands!' + rand_number;
 
 var rand_number = math.random();
 
 function reload() {
-    location.reload();
+    location.reload(true);
 }
